cc12b76e3e772627927f85054dacbadd1ed7187c
Rahul Mistry <22cs042@charusat.edu.in>
2024-09-18 09:49:21 +0530

    trying lint

--- backend/commitlist.config.js
+++ backend/commitlist.config.js
@@ -5,28 +5,19 @@ module.exports = {
       2,
       "always",
       [
-        "feat", // New feature
-        "fix", // Bug fix
-        "docs", // Documentation only changes
-        "style", // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
-        "refactor", // A code change that neither fixes a bug nor adds a feature
-        "perf", // A code change that improves performance
-        "test", // Adding missing tests or correcting existing tests
-        "build", // Changes that affect the build system or external dependencies
-        "ci", // Changes to our CI configuration files and scripts
-        "chore", // Other changes that don't modify src or test files
-        "revert", // Reverts a previous commit
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
       ],
     ],
-    "type-case": [2, "always", "lower-case"],
-    "type-empty": [2, "never"],
-    "subject-case": [2, "always", ["sentence-case", "lower-case"]],
-    "subject-empty": [2, "never"],
-    "subject-full-stop": [2, "never", "."],
-    "header-max-length": [2, "always", 72],
-    "body-leading-blank": [2, "always"],
-    "body-max-line-length": [2, "always", 100],
-    "footer-leading-blank": [1, "always"],
-    "footer-max-line-length": [2, "always", 100],
+    "subject-case": [2, "always", "sentence-case"],
   },
 };
